07415075d93427c1fffc2db5e3a116b8f1a8090a
GuadaG <gueretag@gmail.com>
2017-08-10 18:40:24 -0300

    cambios

--- TP2/TP2.py
+++ TP2/TP2.py
@@ -26,7 +26,7 @@ class Simulacion(object):
         self.TMServidor3 = 5.0
         self.TMServidor4n = 5.0
         self.TMServidor4d = 8.0
-        self.TMServidor5 = 12.0
+        self.TMServidor5 = 5.0
         self.TiempoUltimoEvento = 0.0
         self.TiempoTotalServidor = []
         self.TiempoTotalSistema = 0
@@ -75,14 +75,12 @@ class Simulacion(object):
             elif self.ProximoEvento == 9:
                 self.partidaS5()
 
-        self.reportes()
+        self.reporte()
 
 
     def tiempos(self):
         self.ProximoEvento = 0
         self.Menor = 1
-        #print ("0  ", self.ListaDeEventos[0])
-        #print ("1  ", self.ListaDeEventos[1])
         for i in range(0, 10):
             #print ("i  ", self.ListaDeEventos[i])
             #print ("menor  ", self.ListaDeEventos[self.Menor])
@@ -92,7 +90,6 @@ class Simulacion(object):
         self.ProximoEvento = self.Menor
         self.Reloj = self.ListaDeEventos[self.Menor]
         # print (self.Menor)
-
 
 
     def arriboS1(self):
@@ -108,11 +105,17 @@ class Simulacion(object):
 
     def partidaS1(self):
         if self.TamanioCola[0] > 0:
-            self.ListaDeEventos[1] = valorExponencial(self.TMServidor1) + self.Reloj
+            tservicio = valorExponencial(self.TMServidor1)
+            self.ListaDeEventos[1] = tservicio + self.Reloj
             self.TamanioCola[0] -= 1
-            self.TiempoTotalServidor[0] += self.Reloj - self.Cola1[0]
+            self.TiempoTotalServidor[0] += self.Reloj - self.Cola1[0] + tservicio
             self.Cola1.pop(0)
             self.CantClientesAtendidos[0] += 1
+            # self.ListaDeEventos[1] = valorExponencial(self.TMServidor1) + self.Reloj
+            # self.TamanioCola[0] -= 1
+            # self.TiempoTotalServidor[0] += self.Reloj - self.Cola1[0]
+            # self.Cola1.pop(0)
+            # self.CantClientesAtendidos[0] += 1
         else:
             self.EstadoServidor[0] = 'D'
             self.ListaDeEventos[1] = 9999.999
@@ -131,19 +134,23 @@ class Simulacion(object):
         else:
             self.Cola2.append(self.Reloj)
             self.TamanioCola[1] += 1
+            self.ListaDeEventos[2] = 9999.999
 
 
     def partidaS2(self):
         if self.TamanioCola[1] > 0:
-            self.ListaDeEventos[3] = valorExponencial(self.TMServidor2) + self.Reloj
-            self.TiempoTotalServidor[1] += self.Reloj - self.Cola2[0]
+            tservicio = valorExponencial(self.TMServidor2)
+            self.ListaDeEventos[3] = tservicio + self.Reloj
+            self.TiempoTotalServidor[1] += self.Reloj - self.Cola2[0] + tservicio
             self.Cola2.pop(0)
             self.TamanioCola[1] -= 1
             self.CantClientesAtendidos[1] += 1
         else:
             self.EstadoServidor[1] = 'D'
             self.ListaDeEventos[3] = 9999.999
-        self.ListaDeEventos[6] = self.Reloj
+        if (self.TamanioCola[4] > 0):
+            self.ListaDeEventos[6] = self.Reloj
+        self.TamanioCola[3] += 1
         self.Tipo = 'S'
 
 
@@ -156,52 +163,56 @@ class Simulacion(object):
         else:
             self.Cola3.append(self.Reloj)
             self.TamanioCola[2] += 1
+            self.ListaDeEventos[4] = 9999.999
 
 
     def partidaS3(self):
         if self.TamanioCola[2] > 0:
-            self.ListaDeEventos[5] = valorExponencial(self.TMServidor3) + self.Reloj
-            self.TiempoTotalServidor[2] += self.Reloj - self.Cola3[0]
+            tservicio = valorExponencial(self.TMServidor3)
+            self.ListaDeEventos[5] = tservicio + self.Reloj
+            self.TiempoTotalServidor[2] += self.Reloj - self.Cola3[0] + tservicio
             self.Cola3.pop(0)
             self.TamanioCola[2] -= 1
             self.CantClientesAtendidos[2] += 1
         else:
             self.EstadoServidor[2] = 'D'
             self.ListaDeEventos[5] = 9999.999
-        self.ListaDeEventos[6] = self.Reloj
+        if (self.TamanioCola[3] > 0):
+            self.ListaDeEventos[6] = self.Reloj
+        self.TamanioCola[4] += 1
         self.Tipo = 'P'
 
 
     def arriboS4(self):
         if self.EstadoServidor[3] == 'D':
-            if (self.TamanioCola[3]>0) and (self.TamanioCola[4]>0):
-                self.EstadoServidor[3] = 'O'
-                self.CantClientesAtendidos[3] += 1
-                self.ListaDeEventos[6] = 9999.999
-                if (random.rand(0, 1) < 0.12) or (random.rand(0, 1) < 0.1):
-                    self.ListaDeEventos[7] = valorExponencial(self.TMServidor4d) + self.Reloj
-                else:
-                    self.ListaDeEventos[7] = valorExponencial(self.TMServidor4n) + self.Reloj
-        else:
-            if(self.Tipo=='S'):
-                self.Cola4s.append(self.Reloj)
-                self.TamanioCola[3] += 1
+            self.EstadoServidor[3] = 'O'
+            self.CantClientesAtendidos[3] += 1
+            self.ListaDeEventos[6] = 9999.999
+            self.TamanioCola[3] -= 1
+            self.TamanioCola[4] -= 1
+            if (random.randrange(0, 100, 1) < 12) or (random.randrange(0, 100, 1) < 10):
+                self.ListaDeEventos[7] = valorExponencial(self.TMServidor4d) + self.Reloj
             else:
-                self.Cola4p.append(self.Reloj)
-                self.TamanioCola[4] += 1
+                self.ListaDeEventos[7] = valorExponencial(self.TMServidor4n) + self.Reloj
+        else:
+            self.Cola4s.append(self.Reloj)
+            self.ListaDeEventos[6] = 9999.999
+            self.Cola4p.append(self.Reloj)
 
 
     def partidaS4(self):
         if (self.TamanioCola[3] > 0) and (self.TamanioCola[4] > 0):
             self.CantClientesAtendidos[3] += 1
-            if (random.rand(0, 1) < 0.12) or (random.rand(0, 1) < 0.1):
-                self.ListaDeEventos[7] = valorExponencial(self.TMServidor4d) + self.Reloj
+            if (random.randrange(0, 100, 1) < 12) or (random.randrange(0, 100, 1) < 10):
+                tservicio = valorExponencial(self.TMServidor4d)
+                self.ListaDeEventos[7] = tservicio + self.Reloj
             else:
-                self.ListaDeEventos[7] = valorExponencial(self.TMServidor4n) + self.Reloj
+                tservicio = valorExponencial(self.TMServidor4n)
+                self.ListaDeEventos[7] = tservicio + self.Reloj
             if self.Cola4s[0] < self.Cola4p[0]:
-                self.TiempoTotalServidor[3] += self.Reloj - self.Cola4s[0]
+                self.TiempoTotalServidor[3] += self.Reloj - self.Cola4s[0] + tservicio
             else:
-                self.TiempoTotalServidor[3] += self.Reloj - self.Cola4p[0]
+                self.TiempoTotalServidor[3] += self.Reloj - self.Cola4p[0] + tservicio
             self.Cola4s.pop(0)
             self.Cola4p.pop(0)
             self.TamanioCola[3] -= 1
@@ -221,17 +232,19 @@ class Simulacion(object):
         else:
             self.Cola5.append(self.Reloj)
             self.TamanioCola[5] += 1
+            self.ListaDeEventos[8] = 9999.999
 
 
     def partidaS5(self):
-        if self.TamanioCola[5] > 0:
-            self.ListaDeEventos[9] = valorExponencial(self.TMServidor5) + self.Reloj
-            self.TiempoTotalServidor[4] += self.Reloj - self.Cola5[0]
+        if (self.TamanioCola[5] > 0):
+            tservicio = valorExponencial(self.TMServidor5)
+            self.ListaDeEventos[9] = tservicio + self.Reloj
+            self.TiempoTotalServidor[4] += self.Reloj - self.Cola5[0] + tservicio
             self.Cola5.pop(0)
             self.TamanioCola[5] -= 1
             self.CantClientesAtendidos[4] += 1
         else:
-            self.EstadoServidor[2] = 'D'
+            self.EstadoServidor[4] = 'D'
             self.ListaDeEventos[9] = 9999.999
 
 
@@ -239,14 +252,12 @@ class Simulacion(object):
         salida = open('TPSimu.csv', 'w')
 
         for i in range (0, 5):
-            salida.write('Tiempo medio en servidor ('+str(i+1) + ');')
-            salida.write(str(self.TiempoTotalServidor[i]/self.CantClientesAtendidos[i]) + ';')
+            salida.write('Tiempo medio en servidor (' + str(i+1) + ');')
+            salida.write(str(self.TiempoTotalServidor[i] / self.CantClientesAtendidos[i]) + ';' + str(
+                self.CantClientesAtendidos[i]))
             salida.write('\n')
             self.TiempoTotalSistema += self.TiempoTotalServidor[i]/self.CantClientesAtendidos[i]
-
-
-##
-
+        salida.write(str(self.TiempoTotalSistema))
 #---------------------------------------------
 # Funciones
 #---------------------------------------------
